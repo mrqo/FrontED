d2cf2396c116f25bfdf606e575a8b89f5a8e0315
drozdowsky <hdrozdow+github@pm.me>
2019-05-18 15:20:02 +0200

    [backend] Add REST

---
+++ backend/wwwdeamon/ed/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class EdConfig(AppConfig):
+    name = 'ed'
---
+++ backend/wwwdeamon/ed/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=128)
+    source = models.TextField(null=False)
+
+    owner = models.ForeignKey('ed.User', on_delete=models.CASCADE,
+                              related_name="projects", null=False)
+
+
+class User(AbstractUser):
+    pass
