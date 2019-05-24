e729ff12473ab058c53a3b8e2cd6c313416f9751
elMarco <elmarcopl@gmail.com>
2019-05-24 18:15:33 +0200

    Fixed container scaling.

--- app/src/modules/Design/components/Elements/ContainerRenderElement.jsx
+++ app/src/modules/Design/components/Elements/ContainerRenderElement.jsx
@@ -2,14 +2,18 @@ import React from 'react';
 import { Group, Rect } from 'react-konva';
 
 class ContainerRenderElement extends React.Component {
+    scaled(val) {
+        return this.props.scale * val;
+    }
+
     render() {
         return (
             <Group>
                 <Rect
                     x={0}
                     y={0}
-                    width={this.props.model.properties.width}
-                    height={this.props.model.properties.height}
+                    width={this.props.width}
+                    height={this.props.height}
                     stroke={"black"}
                     strokeWidth={1}
                     dash={[2,1]}
