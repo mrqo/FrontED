d692697a9c200e33bf2f17e9310c2dd6b2977971
elMarco <elmarcopl@gmail.com>
2019-05-24 18:25:21 +0200

    Fixed image scaling.

--- app/src/modules/Design/components/Elements/ImageRenderElement.jsx
+++ app/src/modules/Design/components/Elements/ImageRenderElement.jsx
@@ -2,27 +2,32 @@ import React from 'react';
 import { Group, Rect, Text, Image } from 'react-konva';
 
 class ImageRenderElement extends React.Component {
+    scaled(val) {
+        return this.props.scale * val;
+    }
+
     render() {
         return (
             <Group>
                 <Rect
                     x={0}
                     y={0}
-                    width={this.props.model.properties.width}
-                    height={this.props.model.properties.height}
+                    width={this.props.width}
+                    height={this.props.height}
                     fill={"red"}/>
                 <Text
                     x={0}
-                    y={0 - 10}
-                    width={this.props.model.properties.width}
-                    height={this.props.model.properties.height}
+                    y={0 - this.scaled(10)}
+                    width={this.props.width}
+                    height={this.props.height}
                     align={"center"}
-                    text={"IMAGE PLACEHOLDER"}/>
+                    text={"IMAGE PLACEHOLDER"}
+                    fontSize={this.scaled(10)}/>
                 <Image
                     x={0}
                     y={0}
-                    width={this.props.model.properties.width}
-                    height={this.props.model.properties.height}
+                    width={this.props.width}
+                    height={this.props.height}
                     src={this.props.model.properties.src}
                     image={this.props.model.properties.image}
                     />
