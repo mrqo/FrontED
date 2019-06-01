efc6d9365d0fbe7a8b39b26068edb2124c5fc758
drozdowsky <hdrozdow+github@pm.me>
2019-06-01 14:20:34 +0200

    login

---
+++ app/src/modules/Domain/Managers/SessionManager.js
@@ -0,0 +1,73 @@
+import config from 'config';
+
+export const userService = {
+    login,
+    logout,
+};
+
+export function authHeader() {
+    // return authorization header with basic auth credentials
+    let user = JSON.parse(localStorage.getItem('user'));
+
+    if (user && user.authdata) {
+        return { 'Authorization': 'Basic ' + user.authdata };
+    } else {
+        return {};
+    }
+}
+
+export function login(username, password) {
+    const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({username, password })
+    };
+
+    return fetch(`127.0.0.1:8000/accounts/login`, requestOptions)
+        .then(handleResponse)
+        .then(user => {
+            // login successful if there's a user in the response
+            console.log(user);
+            if (user) {
+                // store user details and basic auth credentials in local storage
+                // to keep user logged in between page refreshes
+                user.authdata = window.btoa(username + ':' + password);
+                localStorage.setItem('user', JSON.stringify(user));
+            }
+            return user;
+        });
+}
+
+export function logout() {
+    localStorage.removeItem('user');
+}
+
+/*
+// GET example
+function getAll() {
+    const requestOptions = {
+        method: 'GET',
+        headers: authHeader()
+    };
+
+    return fetch(`/users`, requestOptions).then(handleResponse);
+}
+*/
+
+export function handleResponse(response) {
+    return response.text().then(text => {
+        const data = text && JSON.parse(text);
+        if (!response.ok) {
+            if (response.status === 401) {
+                // auto logout if 401 response returned from api
+                logout();
+                location.reload(true);
+            }
+
+            const error = (data && data.message) || response.statusText;
+            return Promise.reject(error);
+        }
+
+        return data;
+    });
+}
