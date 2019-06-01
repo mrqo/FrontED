f6896329b4cc03fc0d8a7d654234e55e38ac643a
drozdowsky <hdrozdow+github@pm.me>
2019-06-01 13:42:32 +0200

    Fix xml

--- backend/wwwdaemon/wwwdaemon/settings.py
+++ backend/wwwdaemon/wwwdaemon/settings.py
@@ -113,14 +113,12 @@ REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
         'rest_framework.renderers.BrowsableAPIRenderer',
-        'rest_framework_xml.renderers.XMLRenderer',
     ),
 
     'DEFAULT_PARSER_CLASSES': (
         'rest_framework.parsers.JSONParser',
         'rest_framework.parsers.FormParser',
         'rest_framework.parsers.MultiPartParser',
-        'rest_framework_xml.parsers.XMLParser',  # FIXME: ?
     ),
 }
 
