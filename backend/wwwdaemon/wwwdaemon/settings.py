a84c76480719f1719130248e3b474189b88e3731
drozdowsky <hdrozdow+github@pm.me>
2019-06-01 15:43:06 +0200

    fix fix fix

--- backend/wwwdaemon/wwwdaemon/settings.py
+++ backend/wwwdaemon/wwwdaemon/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '(wohus55!x#hno=a_h54-hw@$yubhh$5z(#&$y1(4#efl@le%1'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
