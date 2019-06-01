6ee36ba7467919a29f336b2e3a904801a306ea6e
drozdowsky <hdrozdow+github@pm.me>
2019-06-01 14:20:34 +0200

    login

--- app/src/modules/IDE/components/LoginDialog.jsx
+++ app/src/modules/IDE/components/LoginDialog.jsx
@@ -6,6 +6,7 @@ import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
+import login from '../../Domain/Managers/SessionManager';
 
 class LoginDialog extends React.Component {
   render() {
@@ -23,7 +24,6 @@ class LoginDialog extends React.Component {
                 fullWidth
               />
               <TextField
-                autoFocus
                 margin="dense"
                 id="name"
                 label="Password"
