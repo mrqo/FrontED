7196f25af664536dcf9d61f28e67a6128ab5a2a8
elMarco <elmarcopl@gmail.com>
2019-06-01 14:17:21 +0200

    Started working on main menu.

--- app/src/modules/IDE/components/PageBar.jsx
+++ app/src/modules/IDE/components/PageBar.jsx
@@ -6,8 +6,14 @@ import MoreVertIcon from '@material-ui/icons/MoreVert';
 import { withStyles } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import Avatar from '@material-ui/core/Avatar';
+import Popper from '@material-ui/core/Popper';
+import Grow from '@material-ui/core/Grow';
+import ClickAwayListener from '@material-ui/core/ClickAwayListener';
 import deepPurple from '@material-ui/core/colors/deepPurple';
 import Chip from '@material-ui/core/Chip';
+import Paper from '@material-ui/core/Paper';
+import MenuItem from '@material-ui/core/MenuItem';
+import MenuList from '@material-ui/core/MenuList';
 
 import LoginDialog from './LoginDialog';
 
@@ -33,18 +39,49 @@ const styles = theme => ({
 class PageBar extends React.Component {
     state = {
         loginDialogOpen: false,
+        menuOpen: false,
     }
-
+ 
     render() {
         const { classes } = this.props;
-
+        
         return (
             <div className={classes.root}>
                 <Toolbar variant="dense">
-                    <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+                    <IconButton 
+                        className={classes.menuButton} 
+                        color="inherit"
+                        aria-label="Menu"
+                        onClick={this.handleMenuToggle}>
                         <MoreVertIcon/>
                     </IconButton>
 
+                    {
+
+                    /*
+                    <Popper 
+                        open={this.state.menuOpen} 
+                        anchorEl={anchorRef.current} 
+                        transition disablePortal>
+                        {({ TransitionProps, placement }) => (
+                            <Grow
+                            {...TransitionProps}
+                            style={{ transformOrigin: 'center bottom' }}
+                            >
+                            <Paper id="menu-list-grow">
+                                <ClickAwayListener onClickAway={this.handleMenuClose}>
+                                <MenuList>
+                                    <MenuItem onClick={this.handleNewProject}>New</MenuItem>
+                                    <MenuItem onClick={this.handleSaveProject}>Save</MenuItem>
+                                    <MenuItem onClick={this.handleSelectProject}>Select project</MenuItem>
+                                </MenuList>
+                                </ClickAwayListener>
+                            </Paper>
+                            </Grow>
+                        )}
+                    </Popper>
+                    */
+                    }
                     <Typography variant="h6" color="inherit" className={classes.grow}>
                         FrontED
                     </Typography>
@@ -71,7 +108,28 @@ class PageBar extends React.Component {
         )
     }
 
-    handleLoginClose = () => {
+    handleNewProject = (e) => {
+
+    }
+
+    handleSelectProject = (e) => {
+
+    }
+
+    handleSaveProject = (e) => {
+
+    }
+
+    handleMenuToggle = (e) => {
+        //setAnchorEl(event.currentTarget);
+        this.setState({menuOpen: true});
+    }
+
+    handleMenuClose = (e) => {
+        this.setState({menuOpen: false});
+    }
+
+    handleLoginClose = (e) => {
         this.setState({loginDialogOpen: false});
     }
 }
