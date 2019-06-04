69b0807c28e2ef448a8455cde91924740ee2965c
elMarco <elmarcopl@gmail.com>
2019-06-04 15:15:29 +0200

    Added worksheet caption.

--- app/src/modules/Design/components/WorkspaceSheet.jsx
+++ app/src/modules/Design/components/WorkspaceSheet.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { topic } from '../../Domain/Enums/PubSubTopics';
 import { ElementType } from '../../Domain/Enums/Elements.js';
 
-import { Rect } from 'react-konva';
+import { Rect, Group, Text } from 'react-konva';
 //import { Stage, Layer, Rect, Text, Circle, Group, Image } from 'react-konva';
 //import Konva from 'konva';
 
@@ -15,16 +15,28 @@ class WorkspaceSheet extends React.Component {
     const x = 0;
     const y = 0;
     return (
-      <Rect
-        x={cam.transformX(x)}
-        y={cam.transformY(y)}
-        width={cam.scale(this.props.width)}
-        height={cam.scale(this.props.height)}
-        shadowOpacity={0.2}
-        shadowBlur={20}
-        shadowOffset={{ x: 2, y: 2 }}
-        fill={"white"}
-      />
+      <Group>
+        <Text
+          x={cam.transformX(x)}
+          y={cam.transformY(y - 35)}
+          align={"left"}
+          text={"New project (1920x1080)"}
+          fontSize={cam.scale(25)}
+          opacity={0.2}
+          fontFamily={"calibri"}
+          fontStyle={"italic"}/>
+        <Rect
+          x={cam.transformX(x)}
+          y={cam.transformY(y)}
+          width={cam.scale(this.props.width)}
+          height={cam.scale(this.props.height)}
+          shadowOpacity={0.2}
+          shadowBlur={20}
+          shadowOffset={{ x: 2, y: 2 }}
+          fill={"white"}
+        />
+      </Group>
+
     )
   }
 }
